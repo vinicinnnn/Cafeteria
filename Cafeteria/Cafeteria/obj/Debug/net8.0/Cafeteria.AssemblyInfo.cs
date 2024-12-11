bed142209969f6e930221f0d21ab2dd74c3e1213
vinicinnnn <1vinizada@gmail.com>
2024-12-11 13:35:36 -0300

    Nome da pasta

--- Cafeteria/Cafeteria/obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
+++ Cafeteria/Cafeteria/obj/Debug/net8.0/Cafeteria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09929409d746a7c95ec0a417bb282a0e6c051414")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cafeteria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
